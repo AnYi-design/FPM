ee5560f18d1a407d89537a75b7004baa07f36843
cx <871624892@qq.com>
2025-09-01 15:19:01 +0800

    修改按钮group

--- fpm/webapp/ext/controller/GROUP.js
+++ fpm/webapp/ext/controller/GROUP.js
@@ -4,7 +4,7 @@ sap.ui.define([
     'use strict';
 
     return {
-        onApp: function(oEvent) {
+        onGROUP: function(oEvent) {
             MessageToast.show("Custom handler invoked.");
         }
     };
