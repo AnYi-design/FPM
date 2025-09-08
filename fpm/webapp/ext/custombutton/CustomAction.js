d407589dff776e1bacf69c167df949b9e72879a9
cx <871624892@qq.com>
2025-09-08 09:49:04 +0800

    增加dialog窗口

--- fpm/webapp/ext/custombutton/CustomAction.js
+++ fpm/webapp/ext/custombutton/CustomAction.js
@@ -5,7 +5,71 @@ sap.ui.define([
 
 	return {
         onPressChangeAll:function(oEvent){
-            console.log("change all")
+            const oSelect = new sap.m.Select({
+                width: "10rem",
+                items: [
+                    new sap.ui.core.Item({
+                        text: "MRP유형",
+                        key: "MRPType"
+                    }),
+                    new sap.ui.core.Item({
+                        text: "MRP관리자",
+                        key: "MRPController"
+                    }),
+                    new sap.ui.core.Item({
+                        text: "MRP그룹",
+                        key: "MRPGroup"
+                    })
+                ]
+            });
+            const oInput = new sap.m.Input({
+                width: "10rem"
+            });
+            const oDialog = new sap.m.Dialog({
+                title:"Change All",
+                beginButton: new sap.m.Button({
+                    text:"确认",
+                    press:()=>{
+                        sap.m.MessageToast.show("确认");
+                        oDialog.close();
+                    }
+                }),
+                endButton: new sap.m.Button({
+                    text:"关闭",
+                    press:()=>{
+                        oDialog.close();
+                    }
+                }),
+                content: [
+                    new sap.m.HBox({
+                        alignContent: "Center",
+                        alignItems: "Center",
+                        justifyContent: "Center",
+                        items: [
+                            new sap.m.Label({
+                                width: "5rem",
+                                text: "필드선택:",
+                                Design:"Standard"
+                            }),
+                            oSelect
+                        ]
+                    }),
+                    new sap.m.HBox({
+                        alignContent: "Center",
+                        alignItems: "Center",
+                        justifyContent: "Center",
+                        items: [
+                            new sap.m.Label({
+                                width: "5rem",
+                                text: "값:",
+                                Design:"Standard"
+                            }),
+                            oInput
+                        ]
+                    })
+                ]
+            });
+            oDialog.open();
         }
     }
 });
